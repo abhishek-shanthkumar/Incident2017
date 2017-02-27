ea77c02477bcc5a4576a20201e49032b0bb7e268
Abhishek <abhishek.shanthkumar@gmail.com>
2017-02-27 13:01:00 +0530

    Implemented MapActivity

--- app/src/main/java/in/co/inci17/activities/MapActivity.java
+++ app/src/main/java/in/co/inci17/activities/MapActivity.java
@@ -1,16 +1,63 @@
 package in.co.inci17.activities;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
 import in.co.inci17.R;
 
-public class MapActivity extends BaseActivity {
+public class MapActivity extends BaseActivity
+        implements OnMapReadyCallback {
+
+    private static final String TAG = MapActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_map);
         super.onCreateDrawer();
         navigationView.getMenu().getItem(3).setChecked(true);
+
+        ((TextView) findViewById(R.id.tv_title)).setText("Map");
+
+        // Get the SupportMapFragment and register for the callback
+        // when the map is ready for use.
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready for use.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.map_style));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(13.012, 74.795)));
     }
 }
